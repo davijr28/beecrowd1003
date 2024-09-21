071bdc4f63b3a3c04a3462fdd919037a0ce780c3
ADM <ADM@DESKTOP-88AP3BG>
2024-09-21 14:19:00 -0300

    init: início do projeto

---
+++ src/main/java/Beecrowd1003.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+public class Beecrowd1003 {
+    public static void main(String[] args)throws java.lang.Exception {
+        Scanner leitor = new Scanner(System.in);
+        //declarar variáveis
+        int A, B, SOMA;
+        //ler variáveis
+        A = leitor.nextInt();
+        B = leitor.nextInt();
+        
+        SOMA = A + B;
+        //mostrar resultado no console
+        System.out.println("SOMA = " + SOMA);
+    }
+}
